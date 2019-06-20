7ed9f48a03e8a025082ee7ea428210ac2f1e197a
AgentD1 <jamparker7@yahoo.com>
2019-06-20 11:28:32 -0400

    Started working on tilemapping
    Yay

---
+++ Assets/Scripts/Blocks/BlockTile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class BlockTile : Tile {
+    public float Health;
+
+#if UNITY_EDITOR
+    // The following is a helper that adds a menu item to create a RoadTile Asset
+    [MenuItem("Assets/Create/BlockTile")]
+    public static void CreateRoadTile() {
+        string path = EditorUtility.SaveFilePanelInProject("Save Block Tile", "New Block Tile", "Asset", "Save Block Tile", "Assets");
+        if (path == "")
+            return;
+        AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<BlockTile>(), path);
+    }
+#endif
+}
---
+++ Assets/Scripts/Managers/TilemapManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapManager : MonoBehaviour {
+    public static Dictionary<Vector2Int, float> tileHealths;
+    Tilemap t;
+    void Start() {
+        t = GetComponent<Tilemap>();
+    }
+
+    void Update() {
+        
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision) {
+        t.SetTile(t.WorldToCell(collision.GetContact(0).point + collision.GetContact(0).normal * 0.1f), null);
+    }
+}
